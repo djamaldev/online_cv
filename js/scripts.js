0202d29ef7bcc1aae8d3b41b25e0bf3a6fd5d566
djamaldev <34842155+djamaldev@users.noreply.github.com>
2022-07-12 09:39:39 +0200

    bugs fix

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ function reveal_work() {
       var windowHeight = window.innerHeight;
       var elementTop = reveals[i].getBoundingClientRect().top;
       var elementVisible = 150;
-  
+      reveals[i].classList.add("active");
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
       } else {
@@ -30,7 +30,7 @@ function reveal_work() {
     }
   }
   function reveal_activity() {
-    var reveals = document.querySelectorAll("#activity");
+    var reveals = document.querySelectorAll(".footer");
   
     for (var i = 0; i < reveals.length; i++) {
       var windowHeight = window.innerHeight;
@@ -48,32 +48,6 @@ function reveal_work() {
   window.addEventListener("scroll", reveal_work);
   window.addEventListener("scroll", reveal_info);
   window.addEventListener("scroll", reveal_activity);
-
-  $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-    window.addEventListener("scroll", reveal_info);
-  });
 
   $(document).scroll(function(){
     var scroll = $(window).scrollTop();
