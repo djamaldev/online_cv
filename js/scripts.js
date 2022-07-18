a728da16c693204ae3675a9a163e3f704de5dd89
djamaldev <34842155+djamaldev@users.noreply.github.com>
2022-07-18 09:50:41 +0200

    last update

--- js/scripts.js
+++ js/scripts.js
@@ -44,10 +44,26 @@ function reveal_work() {
       }
     }
   }
+  function reveal_activity() {
+    var reveals = document.querySelectorAll("#activity");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
   
   window.addEventListener("scroll", reveal_work);
   window.addEventListener("scroll", reveal_info);
   window.addEventListener("scroll", reveal_contact);
+  window.addEventListener("scroll", reveal_activity);
 
   $(document).scroll(function(){
     var scroll = $(window).scrollTop();
